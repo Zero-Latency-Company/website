5b2320fa924ab76221f03c9400730b32593760d1
besoeasy <besoeasy@users.noreply.github.com>
2022-07-15 07:37:11 +0530

    removed components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,9 @@
 <template>
-	<heads />
-	<servs />
-	<faqs />
+	<div>
+		<div class="container mx-auto max-w-5xl flex flex-col justify-between items-center relative font-mono">
+			<div class="flex flex-col px-6 py-40">
+				<p class="text-3xl my-6 text-center dark:text-white">ZERO LATENCY IS A ORGANISATION THAT DEVELOPS, MANAGES & FUNDS BLOCKCHAIN STARTUPS.</p>
+			</div>
+		</div>
+	</div>
 </template>
-
-<script>
-	import heads from '../components/heads.vue';
-	import servs from '../components/servs.vue';
-	import faqs from '../components/faqs.vue';
-
-	export default {
-		name: 'App',
-		components: {
-			heads,
-			servs,
-			faqs,
-		},
-	};
-</script>
